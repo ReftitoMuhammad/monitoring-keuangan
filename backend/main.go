275c596089bc147f26a87baee3bc67f4dab9fec9
ReftitoMuhammad <reftitoindi.m@gmail.com>
2025-07-24 10:38:31 +0700

    apalagi ya Allah

--- backend/main.go
+++ backend/main.go
@@ -21,14 +21,6 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	// 	os.Getenv("DB_USER"),
-	// 	os.Getenv("DB_PASSWORD"),
-	// 	os.Getenv("DB_HOST"),
-	// 	os.Getenv("DB_PORT"),
-	// 	os.Getenv("DB_NAME"),
-	// )
-
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		log.Fatal("DATABASE_URL environment variable not set")
