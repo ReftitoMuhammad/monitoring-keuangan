f195f502f9782074c96e6b3f1e85b148ab6c3d4e
ReftitoMuhammad <reftitoindi.m@gmail.com>
2025-07-24 22:19:47 +0700

    penambahan port

--- backend/main.go
+++ backend/main.go
@@ -83,6 +83,11 @@ func main() {
 
 	}
 
-	log.Println("Starting server on :8080")
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default untuk lokal
+	}
+
+	log.Println("Starting server on :" + port)
+	router.Run(":" + port)
 }
