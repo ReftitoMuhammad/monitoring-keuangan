2bb7cca00476402ccb22b7db7c44c247cf8802e5
ReftitoMuhammad <reftitoindi.m@gmail.com>
2025-07-24 10:03:46 +0700

    atur ulang cors

--- backend/main.go
+++ backend/main.go
@@ -43,7 +43,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"}, // Izinkan frontend Anda
+		AllowOrigins:     []string{"http://localhost:3000", "https://aturuang-zeta.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
