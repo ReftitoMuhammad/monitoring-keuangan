3ab19d1db75cfd3e9778815ea0a5c5d4dcdbf641
ReftitoMuhammad <reftitoindi.m@gmail.com>
2025-07-29 14:29:34 +0700

    sikit

--- backend/main.go
+++ backend/main.go
@@ -22,7 +22,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://aturuang-zeta.vercel.app"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://aturuang-zeta.vercel.app, "https://aturuang.reftitoindi.my.id"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
